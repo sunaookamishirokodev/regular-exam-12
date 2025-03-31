1a730eadf136b375d3d8815a31f6ccdf912dfb63
sunaookamishirokodev <140150717+sunaookamishirokodev@users.noreply.github.com>
2025-03-31 20:28:45 +0700

    add register form

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,26 @@
-function openModal() {
+function openMessageModal() {
+  document.getElementById('messageModal').classList.remove('hidden');
+  document.getElementById('messageModal').classList.add('flex');
+}
+
+function closeMessageModal() {
+  document.getElementById('messageModal').classList.add('hidden');
+  document.getElementById('messageModal').classList.remove('flex');
+}
+
+// Close modal when clicking outside
+document.getElementById('messageModal').addEventListener('click', function (e) {
+  if (e.target === this) {
+    closeMessageModal();
+  }
+});
+
+function openRegisterModal() {
   document.getElementById('registerModal').classList.remove('hidden');
   document.getElementById('registerModal').classList.add('flex');
 }
 
-function closeModal() {
+function closeRegisterModal() {
   document.getElementById('registerModal').classList.add('hidden');
   document.getElementById('registerModal').classList.remove('flex');
 }
@@ -11,6 +28,6 @@ function closeModal() {
 // Close modal when clicking outside
 document.getElementById('registerModal').addEventListener('click', function (e) {
   if (e.target === this) {
-    closeModal();
+    closeRegisterModal();
   }
 });
